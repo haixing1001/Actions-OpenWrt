61e4c3c68a60e82b84012cdb281c1ee431e751e5
haixing1001 <50311834+haixing1001@users.noreply.github.com>
2023-05-03 14:25:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,9 @@
 #sed -i 's/1.2021.35/2022.05.22/g' feeds/packages/net/smartdns/Makefile
 #sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/bc68d5979ccf4beb2721e1e466539da550236f68/g' feeds/packages/net/smartdns/Makefile
 #sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+./scripts/feeds update packages
+rm -rf feeds/packages/lang/golang
+svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+./scripts/feeds update -a
+./scripts/feeds install -a
